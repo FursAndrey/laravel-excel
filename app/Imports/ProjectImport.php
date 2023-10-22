fdf8b92ca62c18b864152ede11213863c4932433
Furs Andrei <you@example.com>
2023-10-22 17:34:19 +0300

    add validation and insert failed rows

--- app/Imports/ProjectImport.php
+++ app/Imports/ProjectImport.php
@@ -3,12 +3,16 @@
 namespace App\Imports;
 
 use App\Factories\ProjectFactory;
+use App\Models\FailedRow;
 use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Validators\Failure;
 
-class ProjectImport implements ToCollection
+class ProjectImport implements ToCollection, WithValidation, SkipsOnFailure
 {
     /**
     * @param Collection $collection
@@ -45,4 +49,49 @@ class ProjectImport implements ToCollection
         return $array;
     }
 
+    public function rules(): array
+    {
+        return [
+            '0' => 'required|string',
+            '1' => 'required|string',
+            '2' => 'required|integer',
+            '3' => 'nullable|string',
+            '4' => 'nullable|integer',
+            '5' => 'nullable|string',
+            '6' => 'nullable|string',
+            '7' => 'nullable|integer',
+            '8' => 'nullable|string',
+            '9' => 'nullable|integer',
+            '10' => 'nullable|integer',
+            '11' => 'nullable|integer',
+            '12' => 'nullable|integer',
+            '13' => 'nullable|integer',
+            '14' => 'nullable|integer',
+            '15' => 'nullable|string',
+            '16' => 'nullable|numeric',
+        ];
+    }
+
+    public function onFailure(Failure ...$failures)
+    {
+        $tmp = [];
+        foreach($failures as $failure) {
+            if ($failure->row() === 1) {
+                continue;
+            }
+
+            foreach ($failure->errors() as $error) {
+                $tmp[] = [
+                    'key' => $failure->attribute(),
+                    'row' => $failure->row(),
+                    'message' => $error,
+                    'task_id' => 1,
+                ];
+            }
+        }
+
+        if ($tmp != []) {
+            FailedRow::insertRows($tmp);
+        }
+    }
 }
--- app/Models/FailedRow.php
+++ app/Models/FailedRow.php
@@ -17,4 +17,11 @@ class FailedRow extends Model
     ];
 
     protected $table = 'failed_rows';
+
+    public static function insertRows(array $rows)
+    {
+        foreach ($rows as $row) {
+            FailedRow::create($row);
+        }
+    }
 }
